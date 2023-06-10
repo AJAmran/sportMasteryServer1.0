6bc86cb3213b7a11295accce798a335a456a3d5d
Md Amran Hossen <mdamranhossen77@gmail.com>
2023-06-10 18:36:51 +0600

    get payment info

--- index.js
+++ index.js
@@ -240,6 +240,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/payments/:email', authenticateToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const options = { sort: { date: -1 } };
+      const result = await paymentCollection.find(query, options).toArray();
+      res.send(result);
+    });
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
